e469e2db3dae2adcad4d33502d7844d97e9d72a3
Derek <nightwing917@hotmail.com>
2021-01-07 20:10:10 -0700

    working on replace for pause and play button the does not seem to work

--- script.js
+++ script.js
@@ -9,14 +9,19 @@ let isPlaying = false;
 // Play Song
 function playSong() {
     let isPlaying = true;
+    playBtn.classList.replace('fa-play', 'fa-pause');
+    playBtn.setAttribute('title', 'Pause');
     music.play();
 }
 
 // Pause Song
 function pauseSong() {
     let isPlaying = false;
+    playBtn.classList.replace('fa-pause', 'fa-play');
+    playBtn.setAttribute('title', 'Play');
     music.pause();
 }
 
 // Play Or Pause Event Listener
 playBtn.addEventListener('click', () => (isPlaying ? pauseSong() : playSong()));
+
