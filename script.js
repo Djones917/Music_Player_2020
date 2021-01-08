34d264cedd9968d89e9f5330836bf02f8b43d55f
Derek <nightwing917@hotmail.com>
2021-01-07 19:42:14 -0700

    adding play pause event listener

--- script.js
+++ script.js
@@ -2,3 +2,21 @@ const music = document.querySelector('audio');
 const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play');
 const nextBtn = document.getElementById('next');
+
+// Check if playing
+let isPlaying = false;
+
+// Play Song
+function playSong() {
+    let isPlaying = true;
+    music.play();
+}
+
+// Pause Song
+function pauseSong() {
+    let isPlaying = false;
+    music.pause();
+}
+
+// Play Or Pause Event Listener
+playBtn.addEventListener('click', () => (isPlaying ? pauseSong() : playSong()));
