63f795989a7624305f5ad1848d3fe8fd3b6a9b94
Derek <nightwing917@hotmail.com>
2021-01-07 19:17:49 -0700

    grabbing DOM

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-console.log('Connected!');
+const music = document.querySelector('audio');
+const prevBtn = document.getElementById('prev');
+const playBtn = document.getElementById('play');
+const nextBtn = document.getElementById('next');
