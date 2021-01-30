54a5c3d38b705bad51d498eaf2e140502866b3c3
Derek <nightwing917@hotmail.com>
2021-01-29 19:35:20 -0700

    deleted console logs

--- script.js
+++ script.js
@@ -99,25 +99,21 @@ function updateProgressBar(e) {
        const progressPercent = (currentTime / duration) * 100;
        progress.style.width = `${progressPercent}%`;
        // Calculate display for duration
-       const durationMinutes = Math.floor(duration / 60);
-       console.log('minutes', durationMinutes);
+       const durationMinutes = Math.floor(duration / 60);       
        let durationSeconds = Math.floor(duration % 60);
        if (durationSeconds < 10) {
           durationSeconds = `0${durationSeconds}`;
-       }
-       console.log('seconds', durationSeconds);       
+       }             
        // Delay switching duration element to avoid NAN
        if (durationSeconds) {
         durationEl.textContent = `${durationMinutes}:${durationSeconds}`;
        }
        // Calculate display for duration
-       const currentMinutes = Math.floor(currentTime / 60);
-       console.log('minutes', currentMinutes);
+       const currentMinutes = Math.floor(currentTime / 60);       
        let currentSeconds = Math.floor(currentTime % 60);
        if (currentSeconds < 10) {
           currentSeconds = `0${currentSeconds}`;
-       }
-       console.log('seconds', currentSeconds); 
+       }        
        currentTimeEl.textContent = `${currentMinutes}:${currentSeconds}`;
     }
 }
