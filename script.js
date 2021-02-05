460fe2ea23e7ca11e57eae20f015d9345515b376
Derek <nightwing917@hotmail.com>
2021-02-04 19:09:49 -0700

    all music functionality works

--- script.js
+++ script.js
@@ -120,18 +120,16 @@ function updateProgressBar(e) {
 
 // Set Progress Bar
 function setProgressBar(e) {  
-   const width = this.clientWidth;
-   console.log('width', width);
-   const clickX = e.offsetX;
-   console.log('clickX', clickX);
-   const { duration } = music;
-   console.log(clickX / width);
-   console.log((clickX / width) * duration);
+   const width = this.clientWidth;   
+   const clickX = e.offsetX;   
+   const { duration } = music;  
+   music.currentTime = (clickX / width) * duration;
 }
 
 
 // Event Listeners
 prevBtn.addEventListener('click', prevSong);
 nextBtn.addEventListener('click', nextSong);
+music.addEventListener('ended', nextSong);
 music.addEventListener('timeupdate', updateProgressBar);
 progressContainer.addEventListener('click', setProgressBar);
